a7bd2d5ffc42c19e327dbc0e57976767ecf73eb4
kubikub <36964054+kubikub@users.noreply.github.com>
2025-02-11 16:59:33 +0100

    optimize code

--- apps/centerface.py
+++ apps/centerface.py
@@ -1,5 +1,4 @@
 from functools import lru_cache
-from typing import Tuple
 import numpy as np
 import cv2
 from apps.utils import resource_path, draw_detections
@@ -36,18 +35,18 @@ class CenterFace:
                 import onnx
                 import onnxruntime
                 backend = 'onnxrt'
-            except:
+            except (ImportError, ModuleNotFoundError) as e:
+                self.logger.info(e)
                 # TODO: Warn when using a --verbose flag
                 # self.logger.info('Failed to import onnx or onnxruntime. Falling back to slower OpenCV backend.')
                 backend = 'opencv'
         self.backend = backend
 
-
         if self.backend == 'opencv':
             self.net = cv2.dnn.readNetFromONNX(onnx_path)
         elif self.backend == 'onnxrt':
-            import onnx
-            import onnxruntime
+            # import onnx
+            # import onnxruntime
 
             # Silence warnings about unnecessary bn initializers
             onnxruntime.set_default_logger_severity(3)
@@ -198,6 +197,7 @@ class CenterFace:
                     suppressed[j] = True
         keep = np.nonzero(suppressed == 0)[0]
         return keep
+
     def anonymize_frame(self, dets, frame, mask_scale,
                         replacewith, ellipse, draw_scores, replaceimg, mosaicsize
                         ):
@@ -206,70 +206,3 @@ class CenterFace:
             x1, y1, x2, y2 = boxes.astype(int)
             frame = draw_detections(frame, x1, y1, x2, y2, replacewith, mask_scale, ellipse, mosaicsize)
         return frame
-            # x1, y1, x2, y2 = self.scale_bb(x1, y1, x2, y2, mask_scale)
-            # # Clip bb coordinates to valid frame region
-            # y1, y2 = max(0, y1), min(frame.shape[0] - 1, y2)
-            # x1, x2 = max(0, x1), min(frame.shape[1] - 1, x2)
-            # self.draw_det(
-            #     frame, score, i, x1, y1, x2, y2,
-            #     replacewith=replacewith,
-            #     ellipse=ellipse,
-            #     draw_scores=draw_scores,
-            #     replaceimg=replaceimg,
-            #     mosaicsize=mosaicsize
-            # )
-
-    # def scale_bb(self, x1, y1, x2, y2, mask_scale=1.0):
-    #     s = mask_scale - 1.0
-    #     h, w = y2 - y1, x2 - x1
-    #     y1 -= h * s
-    #     y2 += h * s
-    #     x1 -= w * s
-    #     x2 += w * s
-    #     return np.round([x1, y1, x2, y2]).astype(int)
-
-    # def draw_det(self,
-    #              frame, score, det_idx, x1, y1, x2, y2,
-    #              replacewith: str = 'blur',
-    #              ellipse: bool = True,
-    #              draw_scores: bool = False,
-    #              ovcolor: Tuple[int] = (0, 0, 0),
-    #              replaceimg=None,
-    #              mosaicsize: int = 20):
-    #     if replacewith == 'solid':
-    #         cv2.rectangle(frame, (x1, y1), (x2, y2), ovcolor, -1)
-    #     elif replacewith == 'blur':
-    #         bf = 2  # blur factor (number of pixels in each dimension that the face will be reduced to)
-    #         blurred_box = cv2.blur(
-    #             frame[y1:y2, x1:x2],
-    #             (abs(x2 - x1) // bf, abs(y2 - y1) // bf)
-    #         )
-    #         if ellipse:
-    #             roibox = frame[y1:y2, x1:x2]
-    #             # Get y and x coordinate lists of the "bounding ellipse"
-    #             ey, ex = skimage.draw.ellipse((y2 - y1) // 2, (x2 - x1) // 2, (y2 - y1) // 2, (x2 - x1) // 2)
-    #             roibox[ey, ex] = blurred_box[ey, ex]
-    #             frame[y1:y2, x1:x2] = roibox
-    #         else:
-    #             frame[y1:y2, x1:x2] = blurred_box
-    #     elif replacewith == 'img':
-    #         target_size = (x2 - x1, y2 - y1)
-    #         resized_replaceimg = cv2.resize(replaceimg, target_size)
-    #         if replaceimg.shape[2] == 3:  # RGB
-    #             frame[y1:y2, x1:x2] = resized_replaceimg
-    #         elif replaceimg.shape[2] == 4:  # RGBA
-    #             frame[y1:y2, x1:x2] = frame[y1:y2, x1:x2] * (1 - resized_replaceimg[:, :, 3:] / 255) + resized_replaceimg[:, :, :3] * (resized_replaceimg[:, :, 3:] / 255)
-    #     elif replacewith == 'mosaic':
-    #         for y in range(y1, y2, mosaicsize):
-    #             for x in range(x1, x2, mosaicsize):
-    #                 pt1 = (x, y)
-    #                 pt2 = (min(x2, x + mosaicsize - 1), min(y2, y + mosaicsize - 1))
-    #                 color = (int(frame[y, x][0]), int(frame[y, x][1]), int(frame[y, x][2]))
-    #                 cv2.rectangle(frame, pt1, pt2, color, -1)
-    #     elif replacewith == 'none':
-    #         pass
-    #     if draw_scores:
-    #         cv2.putText(
-    #             frame, f'{score:.2f}', (x1 + 0, y1 - 20),
-    #             cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 255, 0)
-    #         )
